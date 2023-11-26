f800904b7055fb6939012f8e2ece6af8a97271ac
Milka1801 <102184919+Milka1801@users.noreply.github.com>
2023-11-26 16:57:09 +0100

    summary
    first month :(

---
+++ login/script.js
@@ -0,0 +1,25 @@
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+});
+
+var password = document.getElementById("password")
+  , confirm_password = document.getElementById("confirm_password");
+
+function validatePassword(){
+  if(password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Passwords Don't Match");
+  } else {
+    confirm_password.setCustomValidity('');
+  }
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
---
+++ menu/script_sidebar.js
@@ -0,0 +1,16 @@
+
+        document.getElementById('menu-button').addEventListener('click', function () {
+            var sidebar = document.getElementById('sidebar');
+            var menuIcon = document.getElementById('menu-icon');
+            var closeIcon = document.getElementById('close-icon');
+
+            if (sidebar.style.left === "-250px") {
+                sidebar.style.left = "0";
+                menuIcon.style.display = "none";
+                closeIcon.style.display = "block";
+            } else {
+                sidebar.style.left = "-250px";
+                menuIcon.style.display = "block";
+                closeIcon.style.display = "none";
+            }
+        });
